d726f75fb84b5d33f14427cd7f2ca12c9e933652
Victor Almeida <venenohl18@gmail.com>
2025-06-19 19:28:36 -0300

    first
    first

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    fullscreen: true,
+    frame: true,
+    icon: path.join(__dirname, 'icon.ico'),
+    webPreferences: {
+      devTools: false,
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  // Protege contra atalho F12 e Ctrl+Shift+I
+  win.webContents.on('before-input-event', (event, input) => {
+    if (
+      input.key.toLowerCase() === 'i' && input.control && input.shift ||
+      input.key === 'F12'
+    ) {
+      event.preventDefault();
+    }
+  });
+
+  win.loadURL('https://veneno.shop');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
